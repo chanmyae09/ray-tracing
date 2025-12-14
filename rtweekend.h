bd8fdb9beea7a104bf1fca6d51a927ba4f207d31
Chan Aung <charles2002mdy@gmail.com>
2025-12-14 13:23:35 -0800

    Remove multithreading, keep BVH-only implementation

--- rtweekend.h
+++ rtweekend.h
@@ -22,17 +22,12 @@ inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
-// inline double random_double() {
-//     // Returns a random real in [0,1).
-//     return std::rand() / (RAND_MAX + 1.0);
-// }
-
-
 inline double random_double() {
-    thread_local static std::mt19937 generator(std::random_device{}());
-    thread_local static std::uniform_real_distribution<double> distribution(0.0, 1.0);
-    return distribution(generator);
+    // Returns a random real in [0,1).
+    return std::rand() / (RAND_MAX + 1.0);
 }
+
+
 
 inline double random_double(double min, double max) {
     // Returns a random real in [min,max).
